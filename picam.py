124f76d06df91d9ce504c3458b83e4c7d5713e5d
mflasko <mflasko@outlook.com>
2024-08-17 23:10:03 -0700

    Update picam.py

--- picam.py
+++ picam.py
@@ -56,7 +56,7 @@ class OpenCVCapture(object):
 		
 
 		#config.HAAR_FACES
-		haar_cascade = cv2.CascadeClassifier('Haarcascade_frontalface_default.xml') 
+		haar_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml') 
 		faces_rect = haar_cascade.detectMultiScale(gray_img, 1.1, 9) 
 		for (x, y, w, h) in faces_rect: 
 			cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
