dee16d8f2e49b5d64e646855cd9479f39dbf3fec
mflasko <mflasko@outlook.com>
2024-08-17 22:26:54 -0700

    Update picam.py

--- picam.py
+++ picam.py
@@ -33,9 +33,17 @@ class OpenCVCapture(object):
 		#picam2.preview_configuration.main.format = "RGB888"
 		picam2.start()
 		time.sleep(1)
-		img_array = picam2.capture_array()  #returns numpy array 
-		print("DEBUG: captured image array")		
+		#img_array = picam2.capture_array()  #returns numpy array 
+		picam2.capture_file("capture.jpg")
 		picam2.stop()
+		print("DEBUG: captured image from camera")
+		img = cv2.imread('capture.jpg')
+		print("DEBUG: cv2 read")
+		print("DEBUG: image show called")
+		cv2.imshow('file from camera', img)
+		input ('press any key...')
+				
+		
 
 		#data = np.fromstring(data.getvalue(), dtype=np.uint8)
 		# Decode the image data and return an OpenCV image.
