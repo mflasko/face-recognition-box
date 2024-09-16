bfa7c78952461fb74f334081b8585cfcef59a3a7
mflasko <mflasko@outlook.com>
2024-09-15 22:18:31 -0700

    Update test.py

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     print ('running box')
     print ("Press button to lock (if unlocked), or unlock if the correct face is detected.")
     print ("Press Ctrl-C to quit.")
-    green = LED(17)
+    green = LED(27)
 
     while True:
         green.on()
