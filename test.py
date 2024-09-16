fed9f2764a4a49ebf433d7813b6f29c1900feab9
mflasko <mflasko@outlook.com>
2024-09-15 21:57:50 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,27 +1,18 @@
 import time
-from gpiozero import Button, LED, Servo
-import gpiod
+from gpiozero import Button, LED
+
 global tp
 
 if __name__ == '__main__':
     print ('running box')
     print ("Press button to lock (if unlocked), or unlock if the correct face is detected.")
     print ("Press Ctrl-C to quit.")
-    green = LED(22)
-    
-    LED_PIN = 17
-    chip = gpiod.Chip('gpiochip4')
-    led_line = chip.get_line(LED_PIN)
-    led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
-    led_line.set_value(1)	
-
-    print('set value of gpio 17 . sleep for 10 sec')
-    time.sleep(10)
+    green = LED(17)
 
     while True:
         green.on()
         print('green on')
-        time.sleep(3)
+        time.sleep(5)
         green.off()
         print('green off')
 
