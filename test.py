df0baec33e62934f06209da2edc1d77d0ed1e0e0
mflasko <mflasko@outlook.com>
2024-09-15 21:34:32 -0700

    Update test.py

--- test.py
+++ test.py
@@ -1,64 +1,29 @@
-"""Raspberry Pi Face Recognition Treasure Box
-Treasure Box Script
-Copyright 2013 Tony DiCola 
-"""
-#import cv2
 import time
-#import config
-#import face
-#import hardware
 from gpiozero import Button, LED, Servo
-
+import gpiod
 global tp
 
-def button_pressed():
-	tp = time.time()
-	#box.blink_green_led(1)
-
-def button_released():
-	tr = time.time()
-	time_diff = int(tr - tp)
-
-
 if __name__ == '__main__':
-	#camera = config.get_camera()
-	#box = hardware.Box()
-	
-	print ("Running box...")
-	print ("Press button to lock (if unlocked), or unlock if the correct face is detected.")
-	print ("Press Ctrl-C to quit.")
+    print ('running box')
+    print ("Press button to lock (if unlocked), or unlock if the correct face is detected.")
+    print ("Press Ctrl-C to quit.")
+    green = LED(22)
+    
+    LED_PIN = 17
+    chip = gpiod.Chip('gpiochip4')
+    led_line = chip.get_line(LED_PIN)
+    led_line.request(consumer="LED", type=gpiod.LINE_REQ_DIR_OUT)
+    led_line.set_value(1)	
 
-	#box.button.when_pressed = button_pressed
-	#box.button.when_released = button_released
+    print('set value of gpio 17 . sleep for 10 sec')
+    time.sleep(10)
 
-	red = LED(17)
-	green = LED(27)	
-	buttontest = Button(2)
-	print('green led on')
-	green.on()
-	time.sleep(10)
-	print('green off')
-	green.off()
-	print('red LED OFF')
-	red.off()
-	while True:
-		time.sleep(2)
-		print ('looping')
+    while True:
+        green.on()
+        print('green on')
+        time.sleep(3)
+        green.off()
+        print('green off')
 
-'''
-	while True:
-		print('loop started')
-		if buttontest.is_pressed:
-			print("Button is pressed")
-		else:
-			print("Button is not pressed")
-		
-		red.on()
-		time.sleep(1)
-		red.off()
-
-		green.on()
-		time.sleep(1)
-		green.off()
-
-'''
+        
+        
