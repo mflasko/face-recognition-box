6d0e05374823e5d6574006b813fd2407d1087533
mflasko <mflasko@outlook.com>
2024-09-20 13:55:43 -0700

    Update test.py

--- test.py
+++ test.py
@@ -3,11 +3,30 @@ from gpiozero import Button, LED
 
 global tp
 
+#Button.was_held = False
+
+def held(btn):
+    btn.was_held = True
+    print("button was held not just pressed")
+
+def released(btn):
+    if not btn.was_held:
+        pressed()
+    btn.was_held = False
+
+def pressed():
+    print("button was pressed not held")
+
 if __name__ == '__main__':
     print ('running box')
     print ("Press button to lock (if unlocked), or unlock if the correct face is detected.")
     print ("Press Ctrl-C to quit.")
     green = LED(27)
+    btn = Button(27)
+    btn.was_held = False
+    
+    btn.when_held = held
+    btn.when_released = released
 
     while True:
         green.on()
